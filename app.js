e6a08efa51e78f7686211f2c65972cb33215ce2f
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2022-11-11 00:04:36 -0300

    ANIMACION Y LAZY 2

--- app.js
+++ app.js
@@ -44,3 +44,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
